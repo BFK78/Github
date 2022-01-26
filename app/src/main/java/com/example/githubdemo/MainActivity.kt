e70256ca488c4824568d5090e68d2c84af86d40d
Mohammed Basim <basimbfk781@gmail.com>
2022-01-26 11:11:53 +0530

    This is our second commit

--- app/src/main/java/com/example/githubdemo/MainActivity.kt
+++ app/src/main/java/com/example/githubdemo/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.githubdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.githubdemo.ui.theme.GithubDemoTheme
 
@@ -16,7 +21,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             GithubDemoTheme {
                 Surface(color = MaterialTheme.colors.background) {
+                    Box(
+                        modifier = Modifier.fillMaxSize()
+                            .background(Color.Blue)
+                    ) {
 
+                    }
                 }
             }
         }
