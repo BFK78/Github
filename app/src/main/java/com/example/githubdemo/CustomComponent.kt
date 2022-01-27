acc49bba1cc3c36994860f7bb0526d037d3bab11
Mohammed Basim <basimbfk781@gmail.com>
2022-01-27 10:35:04 +0530

    added new file for customcomponent

---
+++ app/src/main/java/com/example/githubdemo/CustomComponent.kt
@@ -0,0 +1,13 @@
+package com.example.githubdemo
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomComponent() {
+
+}
+
+@Composable
+fun CustomComponentPreview() {
+
+}
--- app/src/main/java/com/example/githubdemo/MainActivity.kt
+++ app/src/main/java/com/example/githubdemo/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.example.githubdemo
 
+import android.graphics.Path
+import android.graphics.RectF
+import android.graphics.Typeface
+import android.graphics.Typeface.BOLD
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.githubdemo.ui.theme.GithubDemoTheme
 
@@ -24,22 +36,9 @@ class MainActivity : ComponentActivity() {
                     Box(
                         modifier = Modifier
                             .fillMaxSize()
-                            .background(Color.Blue)
+                            .background(Color.White),
+                        contentAlignment = Alignment.Center
                     ) {
-                        Text(text = "yooo")
-                        Text(text = "2Text View")
-                        Text(text = "fdsfdsfds")
-                        Text(text = "2Text View")
-                        Text(text = "uo")
-                        Text(text = "2Text View")
-                        Text(text = "opfdfkd")
-                        Text(text = "2Text View")
-                        Text(text = "new new")
-                        Text(text = "new new")
-                        Text(text = "2Text View")
-                        Text(text = "new new")
-                        Text(text = "new new")
-
 
                     }
                 }
